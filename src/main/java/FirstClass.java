2e069af7c8261ba71a9588e148980274ddbc9fc6
irishsea <iriserggeeva@gmail.com>
2022-08-27 20:47:15 +0300

    First commit

---
+++ src/main/java/FirstClass.java
@@ -0,0 +1,6 @@
+public class FirstClass implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,5 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Zhongli is better than other characters!");
+    }
 }
