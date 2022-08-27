1be9846a5df4dde2842e9b0eb0fd0941f5583979
irishsea <iriserggeeva@gmail.com>
2022-08-27 20:51:38 +0300

    Second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Zhongli is better than other characters!");
+        System.out.println("Diluc is better than other characters!");
     }
 }
