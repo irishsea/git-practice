f99e91c4703d2c3e698df96a9580a5e903d7bbcb
i.sergeeva <i.sergeeva@infomaximum.com>
2022-12-03 22:29:14 +0300

    Создан абстрактный класс персонаж

---
+++ src/main/java/Character.java
@@ -0,0 +1,12 @@
+public abstract class Character {
+
+    private final String region;
+
+    public Character(String region) {
+        this.region = region;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+}
