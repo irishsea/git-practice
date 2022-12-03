b0cd8f86a96bb215fc266cfd59eea3dba3ecf3b4
i.sergeeva <i.sergeeva@infomaximum.com>
2022-12-03 22:06:27 +0300

    Now Sucrose is the best person

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Kaeya is better than other characters!");
+        System.out.println("Sucrose is better than other characters!");
     }
 }
