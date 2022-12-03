7eb6a295c4ef35ec8604c1395712c8b9d467eade
i.sergeeva <i.sergeeva@infomaximum.com>
2022-12-03 22:31:02 +0300

    В класс персонаж добавлено поле имя

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,12 +1,18 @@
 public abstract class Character {
 
     private final String region;
+    private final String name;
 
-    public Character(String region) {
+    public Character(String region, String name) {
         this.region = region;
+        this.name = name;
     }
 
     public String getRegion() {
         return region;
+    }
+
+    public String getName() {
+        return name;
     }
 }
